a0b00a9fb732b819f9a250a4897c413453334bbb
Banula Lakwindu <64958389+banulalakwindu@users.noreply.github.com>
2023-06-17 22:00:33 +0530

    new

--- diskschedule/urls.py
+++ diskschedule/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('algorithm/', include('algorithm.urls')),
+    path('', include('algorithm.urls')),
 ]
