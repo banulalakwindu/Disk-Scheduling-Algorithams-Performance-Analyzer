51eff4557d94abfb50cea93543ad259ad6bddbf4
Banula Lakwindu <64958389+banulalakwindu@users.noreply.github.com>
2023-07-01 21:19:53 +0530

    New

--- algorithm/views.py
+++ algorithm/views.py
@@ -1,5 +1,51 @@
 from django.shortcuts import render
 from .forms import InputForm
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
+
+def generate_chart(dataObject, title):
+    order_of_execution = dataObject.copy()
+    # Create y-coordinates for track numbers
+    y_coords = range(1, len(order_of_execution) + 1)
+    order_of_execution = order_of_execution[::-1]
+
+    # Create a scatter plot
+    plt.scatter(order_of_execution, y_coords)
+
+    # Add arrows indicating the movement between execution points
+    for i in range(len(order_of_execution) - 1):
+        plt.annotate("", xy=(order_of_execution[i + 1], y_coords[i + 1]), xytext=(order_of_execution[i], y_coords[i]),
+                     arrowprops=dict(arrowstyle="<-", color="black"))
+
+    # Add vertical dot lines
+    for x in order_of_execution:
+        plt.vlines(x, ymin=1, ymax=len(order_of_execution),
+                   colors='gray', linestyles='dotted')
+
+    # Customize the chart
+    plt.xlabel('Execution Point', fontsize=10)
+    plt.ylabel('Track Number')
+    plt.title(title, fontsize=16, fontweight='bold', color='green')
+
+    # Set x-ticks to show execution points
+    plt.xticks(order_of_execution, rotation=90)
+
+    # Save the chart to a BytesIO object
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+
+    # Convert the chart image to a base64 string
+    chart_image = base64.b64encode(buffer.getvalue()).decode('utf-8')
+    buffer.close()
+
+    # Clear the current figure
+    plt.clf()
+
+    return chart_image
+
 
 def fcfs(dataObject, hPos):
     dataObjectforFCFS = dataObject.copy()
@@ -11,6 +57,7 @@ def fcfs(dataObject, hPos):
         hPos = dataObjectforFCFS[i]
     fcfsarr.append(hPos)
     return (sum, fcfsarr)
+
 
 def sstf(dataObject, hPos):
     dataObjectforSSTF = dataObject.copy()
@@ -29,6 +76,7 @@ def sstf(dataObject, hPos):
     sstfarr.append(hPos)
     return (sum, sstfarr)
 
+
 def scan(dataObject, hPos, sPos):
     dataObjectforSCAN = dataObject.copy()
     sum = 0
@@ -37,14 +85,15 @@ def scan(dataObject, hPos, sPos):
     dataObjectforSCAN.append(sPos)
     dataObjectforSCAN.sort()
     index = dataObjectforSCAN.index(hPos)
-    for i in range(index,-1,-1):
+    for i in range(index, -1, -1):
         scanarr.append(dataObjectforSCAN[i])
-    for i in range(index+1,len(dataObjectforSCAN)):
+    for i in range(index+1, len(dataObjectforSCAN)):
         scanarr.append(dataObjectforSCAN[i])
-    #total head movement
+    # total head movement
     for i in range(len(scanarr)-1):
         sum += abs(scanarr[i] - scanarr[i+1])
     return (sum, scanarr)
+
 
 def cscan(dataObject, hPos, sPos, ePos):
     dataObjectforCSCAN = dataObject.copy()
@@ -55,14 +104,15 @@ def cscan(dataObject, hPos, sPos, ePos):
     dataObjectforCSCAN.append(ePos)
     dataObjectforCSCAN.sort()
     index = dataObjectforCSCAN.index(hPos)
-    for i in range(index,len(dataObjectforCSCAN)):
+    for i in range(index, len(dataObjectforCSCAN)):
         cscanarr.append(dataObjectforCSCAN[i])
-    for i in range(0,index):
+    for i in range(0, index):
         cscanarr.append(dataObjectforCSCAN[i])
-    #total head movement
+    # total head movement
     for i in range(len(cscanarr)-1):
         sum += abs(cscanarr[i] - cscanarr[i+1])
     return (sum, cscanarr)
+
 
 def clook(dataObject, hPos):
     dataObjectforCLOOK = dataObject.copy()
@@ -71,18 +121,20 @@ def clook(dataObject, hPos):
     dataObjectforCLOOK.append(hPos)
     dataObjectforCLOOK.sort()
     index = dataObjectforCLOOK.index(hPos)
-    for i in range(index,len(dataObjectforCLOOK)):
+    for i in range(index, len(dataObjectforCLOOK)):
         clookarr.append(dataObjectforCLOOK[i])
-    for i in range(0,index):
+    for i in range(0, index):
         clookarr.append(dataObjectforCLOOK[i])
-    #total head movement
+    # total head movement
     for i in range(len(clookarr)-1):
         sum += abs(clookarr[i] - clookarr[i+1])
     return (sum, clookarr)
 
+
 def home(request):
     form = InputForm()
     return render(request, 'home.html', {'form': form})
+
 
 def processing(request):
     if request.method == 'POST':
@@ -103,9 +155,16 @@ def processing(request):
             outArr.append(outputSSTF)
             (outputSCAN, scanarr) = scan(datalist, int(hPos), int(sPos))
             outArr.append(outputSCAN)
-            (outputCSCAN,cscanarr) = cscan(datalist, int(hPos), int(sPos), int(ePos))
+            (outputCSCAN, cscanarr) = cscan(
+                datalist, int(hPos), int(sPos), int(ePos))
             outArr.append(outputCSCAN)
-            (outputCLOOK,clookarr) = clook(datalist, int(hPos))
+            (outputCLOOK, clookarr) = clook(datalist, int(hPos))
             outArr.append(outputCLOOK)
-            best = algoArr[outArr.index(min(outputFCFS, outputSSTF, outputSCAN, outputCSCAN, outputCLOOK))]
-    return render(request, 'processing.html', {'outputFCFS': outputFCFS, 'outputSSTF': outputSSTF, 'outputSCAN': outputSCAN, 'outputCSCAN': outputCSCAN, 'outputCLOOK': outputCLOOK, 'best': best, 'fcfsarr': fcfsarr, 'sstfarr': sstfarr, 'scanarr': scanarr,'cscanarr': cscanarr, 'clookarr': clookarr })
+            best = algoArr[outArr.index(
+                min(outputFCFS, outputSSTF, outputSCAN, outputCSCAN, outputCLOOK))]
+            fcfs_chart = generate_chart(fcfsarr, "FCFS")
+            sstf_chart = generate_chart(sstfarr, "SSTF")
+            scan_chart = generate_chart(scanarr, "SCAN")
+            cscan_chart = generate_chart(cscanarr, "CSCAN")
+            clook_chart = generate_chart(clookarr, "CLOOK")
+    return render(request, 'processing.html', {'outputFCFS': outputFCFS, 'outputSSTF': outputSSTF, 'outputSCAN': outputSCAN, 'outputCSCAN': outputCSCAN, 'outputCLOOK': outputCLOOK, 'best': best, 'fcfsarr': fcfsarr, 'sstfarr': sstfarr, 'scanarr': scanarr, 'cscanarr': cscanarr, 'clookarr': clookarr, 'fcfs_chart': fcfs_chart, 'sstf_chart': sstf_chart, 'scan_chart': scan_chart, 'cscan_chart': cscan_chart, 'clook_chart': clook_chart})
